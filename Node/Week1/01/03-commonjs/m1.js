826cc339b7f5b4471e47f3b8bfc8daee866ba7a9
lxfan1998 <lxfan1998@163.com>
2020-11-10 21:19:45 +0800

    CommonJs模块化规范

---
+++ Node/Week1/01/03-commonjs/m1.js
@@ -0,0 +1,21 @@
+// m1模块
+let name='m1';
+let age=12;
+
+const add =(a,b)=>{
+    return a+b;
+}
+
+function People(name){
+    this.name=name;
+}
+
+class Person{
+    constructor(name){
+        this.name=name;
+    }
+
+    say(){
+        console.log(this.name+',say hi!');
+    }
+}
